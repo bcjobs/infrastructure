c6860279bd53e309de2c7a8c1b9b5ad3784e2d3a
Johnny Oshika <johnnyoshika@gmail.com>
2015-11-22 22:34:22 -0800

    Start Owin in Demo.Web project.

---
+++ Demo.Web/App_Start/OwinConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Owin;
+using Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Web.App_Start
+{
+    public class OwinConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.SendAsync().Wait();
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            //app.UseFacebookAuthentication(new FacebookAuthenticationOptions
+            //{
+            //    AppId = "",
+            //    AppSecret = "",
+            //    Provider = new FacebookAuthProvider()
+            //});
+        }
+    }
+}
--- Demo.Web/Controllers/DefaultController.cs
+++ Demo.Web/Controllers/DefaultController.cs
@@ -11,19 +11,22 @@ using Demo.Web.Test.Services;
 using Logs;
 using System.Diagnostics.Contracts;
 using System.Web;
+using Authentifications;
 
 namespace Demo.Web.Controllers
 {    
     public class DefaultController : ApiController
     {
-        public DefaultController(ILog log)
+        public DefaultController(ILog log, IPasswords passwords)
         {
             Contract.Requires<ArgumentNullException>(log != null);
             Contract.Ensures(Log != null);
             Log = log;
+            Passwords = passwords;
         }
 
         ILog Log { get; }
+        IPasswords Passwords { get; }
 
         class MyEvent : ILoggable
         {
@@ -31,7 +34,10 @@ namespace Demo.Web.Controllers
         }
 
         public async Task<IEnumerable<ILogMessage<object, Exception>>> Get()
-        {            
+        {
+            var password = await Passwords.CreateAsync("johnny");
+
+            await new InvalidOperationException().RaiseAsync();
             return Log.Read(new LogQuery());
         }
     }
