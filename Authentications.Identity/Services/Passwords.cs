468e73d6e8c90c1e8851a6ec417023530d7e0baa
Dmitry Nogin <dmitryn@mail.com>
2015-11-22 21:53:32 -0800

    Extending Logging.

--- Authentications.Identity/Services/Passwords.cs
+++ Authentications.Identity/Services/Passwords.cs
@@ -13,76 +13,38 @@ namespace Authentications.Identity.Services
     {
         public async Task ChangeAsync(string userId, string currentPassword, string newPassword)
         {
-            using (var e = new PasswordChange(userId).Start())
-                try
-                {
-                    var result = await IdentityManagers.UserManager.ChangePasswordAsync(userId, currentPassword, newPassword);
-                    if (!result.Succeeded)
-                        throw new InvalidOperationException(string.Join(", ", result.Errors));                    
-                }
-                catch (Exception ex)
-                {
-                    e.Fail(ex);
-                    throw;
-                }
+            var result = await IdentityManagers.UserManager.ChangePasswordAsync(userId, currentPassword, newPassword);
+            if (!result.Succeeded)
+                throw new InvalidOperationException(string.Join(", ", result.Errors));                    
         }
 
         public async Task<string> CreateAsync(string userId)
         {
-            using (var e = new PasswordCreation(userId).Start())
-                try
-                {
-                    var user = new AuthenticationUser
-                    {
-                        Id = userId,
-                        UserName = userId
-                    };
+            var user = new AuthenticationUser
+            {
+                Id = userId,
+                UserName = userId
+            };
 
-                    string password = new RandomString(8);
-                    var result = await IdentityManagers.UserManager.CreateAsync(user, password);
-                    if (!result.Succeeded)
-                        throw new InvalidOperationException(string.Join(", ", result.Errors));
+            string password = new RandomString(8);
+            var result = await IdentityManagers.UserManager.CreateAsync(user, password);
+            if (!result.Succeeded)
+                throw new InvalidOperationException(string.Join(", ", result.Errors));
 
-                    await e.RaiseAsync();
-                    return password;
-                }
-                catch (Exception ex)
-                {
-                    e.Fail(ex);
-                    throw;
-                }
+            return password;
         }
 
         public async Task<string> CreateTokenAsync(string userId)
         {
-            using (var e = new PasswordTokenCreation(userId).Start())
-                try
-                {
-                    var user = await IdentityManagers.GetOrCreateAsync(userId);
-                    var token = await IdentityManagers.UserManager.GeneratePasswordResetTokenAsync(userId);
-                    return token;
-                }
-                catch (Exception ex)
-                {
-                    e.Fail(ex);
-                    throw;
-                }
+            var user = await IdentityManagers.GetOrCreateAsync(userId);
+            return await IdentityManagers.UserManager.GeneratePasswordResetTokenAsync(userId);            
         }
 
         public async Task ResetAsync(string userId, string token, string password)
         {
-            using (var e = new PasswordReset(userId).Start())
-                try
-                {
-                    var result = await IdentityManagers.UserManager.ResetPasswordAsync(userId, token, password);
-                    if (!result.Succeeded)
-                        throw new InvalidOperationException(string.Join(", ", result.Errors));
-                }
-                catch (Exception ex)
-                {
-                    e.Fail(ex);
-                    throw;
-                }
+            var result = await IdentityManagers.UserManager.ResetPasswordAsync(userId, token, password);
+            if (!result.Succeeded)
+                throw new InvalidOperationException(string.Join(", ", result.Errors));
         }
     }
 }
