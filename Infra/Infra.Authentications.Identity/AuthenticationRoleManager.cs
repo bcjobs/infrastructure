7eebc0939dd1774bd6090b02d2e523bc36def35f
magwayer <maxim.gubin@mail.ru>
2015-11-25 21:45:41 +0300

    Add RoleManagement.

---
+++ Infra/Infra.Authentications.Identity/AuthenticationRoleManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Authentications.Identity
+{
+    public class AuthenticationRoleManager : RoleManager<AuthenticationRole>
+    {
+        public AuthenticationRoleManager(RoleStore<AuthenticationRole> store)
+                : base(store)
+        {
+        }
+
+        public static AuthenticationRoleManager Create(IdentityFactoryOptions<AuthenticationRoleManager> options,
+            IOwinContext context)
+        {
+            return new AuthenticationRoleManager(new RoleStore<AuthenticationRole>(context.Get<AuthenticationContext>()));
+        }
+    }
+}
