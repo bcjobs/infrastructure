e2192883eb130c1f2526326e8d7e0a97e1d0b05d
ma-gu <maxim.gubin@mail.ru>
2015-12-03 18:58:08 +0300

    Add IRoles, extend IUsers in order to keep IdentityManagers internal.

---
+++ Infra.Authentications.Identity/Services/Roles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Authentications.Identity.Services
+{
+    public class Roles : IRoles
+    {
+        public async Task CreateAsync(string name)
+        {
+            var role = new AuthenticationRole(name);
+            var result = await IdentityManagers.RoleManager.CreateAsync(role);
+
+            if (!result.Succeeded)
+                throw new InvalidOperationException(string.Join(", ", result.Errors));
+        }
+    }
+}
