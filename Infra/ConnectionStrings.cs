355dce9273aab46b1212275430d1e935207e50dc
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-10 21:03:47 -0800

    Added Guard and ConnectionStrings

---
+++ Infra/ConnectionStrings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    public class ConnectionStrings
+    {
+        public static ConnectionStrings Settings { get; } = new ConnectionStrings();
+        
+        readonly Dictionary<string, string> _values = new Dictionary<string, string>();
+
+        public ConnectionStrings()
+        {
+            ReadConfig();
+        }
+
+        void ReadConfig()
+        {
+            try
+            {
+                foreach (System.Configuration.ConnectionStringSettings connection in ConfigurationManager.ConnectionStrings)
+                {
+                    this[connection.Name] = connection.ConnectionString;
+                }
+
+            }
+            catch (ConfigurationErrorsException)
+            {
+            }
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                return _values[name];
+            }
+            set
+            {
+                _values[name] = value;
+            }
+        }
+
+    }
+}
---
+++ Infra/Guard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    public static class Guard
+    {
+        public static void Assert<T>(bool condition) where T : Exception
+        { 
+            if (!condition)
+                throw Activator.CreateInstance<T>();
+        }
+
+        public static void Assert<T>(bool condition, string message) where T : Exception
+        {
+            if (!condition)
+                throw (T)Activator.CreateInstance(typeof(T), message);
+        }
+    }
+}
